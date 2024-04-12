d8f7fa9ca7874e1a11ef70d445eb6f3f44809de6
dziubenzo <dziubenzo@gmail.com>
2024-04-12 14:57:01 +0200

    Be able to create account

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,39 @@
 import { StyledRegisterPage } from '../styles/RegisterPage.styled';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import API_URL from '../API.js';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 function RegisterPage() {
-  function register(event) {
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+  async function register(event) {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const newUser = {
+      username: formData.get('username'),
+      password: formData.get('password'),
+      confirm_password: formData.get('confirm_password'),
+    };
+    try {
+      const res = await fetch(`${API_URL}/users`, {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await res.json();
+      if (!res.ok) {
+        throw result;
+      }
+      // Show toast and redirect to login if user created successfully
+      toast.success(result);
+      navigate('/');
+    } catch (error) {
+      setError(error);
+    }
   }
   return (
     <StyledRegisterPage>
@@ -35,6 +65,7 @@ function RegisterPage() {
           maxLength={16}
           required
         />
+        {error && <p className="error-message">{error}</p>}
         <button type="submit">Register</button>
       </form>
       <Link to="/">
