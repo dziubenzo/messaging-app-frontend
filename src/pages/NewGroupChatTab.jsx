64e63c3da08cc44468ef25b3a9f99e2fbd7d65ad
dziubenzo <dziubenzo@gmail.com>
2024-04-23 16:37:20 +0200

    Implement creating new group chats

--- src/pages/NewGroupChatTab.jsx
+++ src/pages/NewGroupChatTab.jsx
@@ -1,4 +1,5 @@
-import { useOutletContext } from 'react-router-dom';
+import { useNavigate, useOutletContext } from 'react-router-dom';
+import API_URL from '../API';
 import {
   StyledForm,
   StyledNameInputField,
@@ -8,21 +9,46 @@ import {
 import { toast } from 'react-toastify';
 
 function NewGroupChatTab() {
+  const navigate = useNavigate();
   const { user, contacts } = useOutletContext();
 
-  function createGroupChat(event) {
+  // Create new group chat if there are at least two members selected
+  async function createGroupChat(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const members = formData.getAll('contact');
     if (members.length < 2) {
-      toast.error('You need to select at least two contacts');
+      return toast.error('You need to select at least two contacts');
     }
     members.push(user._id);
-    const newGroupChat = {
+    const newChat = {
       name: formData.get('name'),
       created_by: user._id,
       members,
     };
+    const res = await fetch(`${API_URL}/group-chats/`, {
+      method: 'POST',
+      body: JSON.stringify(newChat),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+    if (!res.ok) {
+      toast.error('Creating group chat failed');
+      return;
+    }
+    const newGroupChat = await res.json();
+    toast.success('Group chat created successfully!');
+    return navigate('/home/group-chats');
+  }
+
+  if (!contacts.length) {
+    return (
+      <div className="no-contacts-wrapper">
+        <p>Add some contacts first!</p>
+      </div>
+    );
   }
 
   return (
@@ -40,6 +66,7 @@ function NewGroupChatTab() {
       </StyledNameInputField>
       <p>Contacts (at least 2):</p>
       <StyledContacts>
+        {!contacts.length && <p>Add some contacts first!</p>}
         {contacts.map((contact) => {
           return (
             <StyledContactCheckbox key={contact.user_id}>
