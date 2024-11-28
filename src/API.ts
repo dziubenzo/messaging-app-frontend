3773530d12576f296b1d0a0bf17e23868935fa21
dziubenzo <dziubenzo@gmail.com>
2024-11-28 13:33:21 +0100

    Fix wrong API endpoint

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,4 @@
-// const API_URL = 'https://dziubenzo-messaging-app-backend.fly.dev';
-const API_URL = 'http://192.168.0.13:3000';
+const API_URL = 'https://dziubenzo-messaging-app-backend.fly.dev';
+// const API_URL = 'http://192.168.0.13:3000';
 
 export default API_URL;
